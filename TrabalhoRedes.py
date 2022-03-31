8d89bf9911f5d568c0a256c0ab394e3757b6bd27
kessystaub <51220952+kessystaub@users.noreply.github.com>
2022-03-31 16:22:12 -0300

    Update TrabalhoRedes.py

--- TrabalhoRedes.py
+++ TrabalhoRedes.py
@@ -5,7 +5,6 @@ def xor(chave,binario):
     if binario[0] =='0':
         for i in range (1, len(binario)):
             resultado.append(str(binario[i]))
-        
         return ''.join(resultado)
     else:    
         for i in range(1,len(binario)):   
@@ -16,7 +15,6 @@ def xor(chave,binario):
         return ''.join(resultado)
 
 def verifica_zeros(aux,binario,fim):
-    
     aux2=[]
     aux2.append(aux[1])
     aux2.append(aux[2])
@@ -29,17 +27,15 @@ def verifica_zeros(aux,binario,fim):
         aux2[2] = aux2[3]
         aux2[3] = binario[fim]
         fim += 1
-    
     return ''.join(aux2),fim
 
 def processo(binario,chave):
     #binario =1111111111111110000
     #chave =  1001
-    x=0
-    inicio=0
+    inicio = 0
     fim = 4
     aux = binario[inicio:fim]
-    #while len(binario) > len(chave)
+
     while fim < 19:
         if aux[0]=='0':
             resultado,fim=verifica_zeros(aux,binario,fim)
@@ -47,7 +43,6 @@ def processo(binario,chave):
             resultado=aux         
         aux = xor(chave,resultado) +  binario[fim]
         fim+=1
-    
     
     resultado=aux         
     aux = xor(chave,resultado)
@@ -58,7 +53,7 @@ def codificar(binario,chave):
     t_chave = len(chave)
     binario_sr= binario + '0'*(t_chave-1)
     resto = processo(binario_sr,chave)
-    print(resto)
+    print('Resto após calculo de codificaçao: ',resto)
     binario=binario+resto
     
     return binario
@@ -75,7 +70,6 @@ def simular_erro(binario):
     binario_corrompido=''
     if chance_erro == (len(binario)-2):
         for i in range(0,len(binario)):
-
             if i== 1:
                 binario_corrompido=binario_corrompido+'0'
             else:
@@ -87,24 +81,22 @@ def simular_erro(binario):
 def simular():
     binario = '1111111111111110'
     chave = '1001'
-    print('Dado sendo enviado : ', binario)
-    print('Chave : ', chave)
+    print('Dado sendo enviado: ', binario)
+    print('Chave: ', chave)
 
     
-    print('codificado!')
+    print('Dado Codificado!')
     binario_cod=codificar(binario,chave)
-    print('Dado codificado : ', binario_cod)
-    
+    print('Dado codificado: ', binario_cod)
 
     binario_cod=simular_erro(binario_cod)
-    
 
     binario_decod,resto=decodificar(binario_cod,chave)
-    print('Dado decodificado',binario_decod)
-    print('Resto ', resto)
+    print('Dado decodificado: ',binario_decod)
+    print('Resto do calculo: ', resto)
     if(resto == '000'):
-        print('mensagem sem erros')
+        print('Mensagem sem erros!')
     else:
-        print('mensagem corrompida favor reenviar a mensagem ')
+        print('Mensagem corrompida! Favor reenviar a mensagem.')
 
 print(simular())
